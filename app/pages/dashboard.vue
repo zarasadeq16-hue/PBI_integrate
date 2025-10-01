7958ee98a493d1a5d1bb1d3d805e4bbc939c720b
zarasadeq16-hue <zarasadeq16@gmail.com>
2025-10-01 16:43:34 -0500

    Create dashboard.vue

---
+++ app/pages/dashboard.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Power BI Dashboard</h1>
+    <iframe
+      width="800"
+      height="600"
+      src="https://app.powerbi.com/view?r=YOUR-DASHBOARD-ID"
+      frameborder="0"
+      allowfullscreen
+    ></iframe>
+  </div>
+</template>
