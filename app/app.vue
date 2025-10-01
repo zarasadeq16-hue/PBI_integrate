c2c3959353872cec8c750567b68c68ab645d9540
zarasadeq16-hue <zarasadeq16@gmail.com>
2025-10-01 16:43:30 -0500

    Create app.vue

---
+++ app/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <nav>
+      <NuxtLink to="/">Home</NuxtLink> |
+      <NuxtLink to="/dashboard">Dashboard</NuxtLink>
+    </nav>
+
+    <NuxtPage />  <!-- This is where pages render -->
+  </div>
+</template>
+
